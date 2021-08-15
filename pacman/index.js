19df3d00af6c1511c42589ac68e91e319f71214d
lanejm <67156874+lanejm@users.noreply.github.com>
2021-08-15 18:08:20 -0400

    added score display

--- pacman/index.js
+++ pacman/index.js
@@ -2,6 +2,7 @@ const width = 28;
 const grid = document.querySelector(".grid");
 const scoreDisplay = document.getElementById("score");
 let squares = [];
+let score = 0;
 //28 * 28 = 784
 // 0 - pac-dots
 // 1 - wall
@@ -101,6 +102,10 @@ function control(e) {
         pacmanCurrentIndex % width !== 0
       )
         pacmanCurrentIndex -= 1;
+      //pacman shortcut
+      if (pacmanCurrentIndex === 364) {
+        pacmanCurrentIndex = 391;
+      }
       break;
     case 39:
       if (
@@ -109,8 +114,21 @@ function control(e) {
         pacmanCurrentIndex % width < width - 1
       )
         pacmanCurrentIndex += 1;
+      //pacman shortcut
+      if (pacmanCurrentIndex === 391) {
+        pacmanCurrentIndex = 364;
+      }
   }
   squares[pacmanCurrentIndex].classList.add("pacman");
+  pacDotEaten();
 }
 
 document.addEventListener("keyup", control);
+
+function pacDotEaten() {
+  if (squares[pacmanCurrentIndex].classList.contains("pac-dot")) {
+    squares[pacmanCurrentIndex].classList.remove("pac-dot");
+    score++;
+    scoreDisplay.innerHTML = score;
+  }
+}
