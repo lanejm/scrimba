edeb7fb323f08fa9e8ef0ec4f907bbb9e7101967
lanejm <67156874+lanejm@users.noreply.github.com>
2021-08-16 08:01:10 -0400

    refactoring

--- pacman/index.js
+++ pacman/index.js
@@ -188,7 +188,6 @@ function moveGhost(ghost) {
   console.log(direction);
 
   ghost.timerId = setInterval(function () {
-    //all our code
     //if the next square does NOT contain a wall and does not contain a ghost
     if (
       !squares[ghost.currentIndex + direction].classList.contains("wall") &&
