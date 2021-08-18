320a80a964ddfe468cc1e01a3aac47509e42270f
lanejm <67156874+lanejm@users.noreply.github.com>
2021-08-18 12:13:16 -0400

    bored bot finished

--- boredBot/index.js
+++ boredBot/index.js
@@ -1,8 +1,11 @@
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    document.getElementById(
-      "image-container"
-    ).innerHTML = `<img src="${data.message}" />`;
-  });
+const button = document.getElementById("btn");
+
+button.addEventListener("click", function () {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("random-idea").textContent = data.activity;
+      document.getElementById('title').textContent = "Here's an idea";
+      document.body.classList.add("fun")
+    });
+});
