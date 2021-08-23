118ceb5adb9548adda20ea1841b1ac798a240ee7
lanejm <67156874+lanejm@users.noreply.github.com>
2021-08-23 08:14:32 -0400

    post request

--- blogSpace/index.js
+++ blogSpace/index.js
@@ -1,25 +1,29 @@
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((response) => response.json())
   .then((data) => {
-    const postsArr = data.slice(0,5)
-    let html = ""
+    const postsArr = data.slice(0, 5);
+    let html = "";
     for (let post of postsArr) {
-        html += `
+      html += `
         <h3>${post.title}</h3>
         <p>${post.body}</p>
         <hr />
-        `
+        `;
     }
-    document.getElementById('blog-list').innerHTML = html
+    document.getElementById("blog-list").innerHTML = html;
   });
 
-
-document.getElementById('new-post').addEventListener('submit', function(e) {
-  e.preventDefault()
-  const postTitle = document.getElementById('post-title').value
-  const postBody = document.getElementById('post-body').value;
+document.getElementById("new-post").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const postTitle = document.getElementById("post-title").value;
+  const postBody = document.getElementById("post-body").value;
   const data = {
     title: postTitle,
-    body: postBody
-  }
-})
+    body: postBody,
+  };
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", { 
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {'Content-Type': 'application/json'}
+   });
+});
