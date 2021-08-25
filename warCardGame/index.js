1a9469bfa44a51c39b0b933ede7523f79de413b1
lanejm <67156874+lanejm@users.noreply.github.com>
2021-08-25 15:45:14 -0400

    callback function added

--- warCardGame/index.js
+++ warCardGame/index.js
@@ -1,6 +1,7 @@
-document.getElementById("new-deck").addEventListener('click', function(e) {
-    e.preventDefault();
+function handleClick() {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
     .then(response => response.json())
     .then(data => console.log(data))
-})
+}
+
+document.getElementById("new-deck").addEventListener('click', handleClick)
