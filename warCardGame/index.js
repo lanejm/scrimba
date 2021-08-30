17cdebbd7ff9152aea797d2c390cd2bc2e917aee
lanejm <67156874+lanejm@users.noreply.github.com>
2021-08-30 09:05:20 -0400

    deckId

--- warCardGame/index.js
+++ warCardGame/index.js
@@ -1,7 +1,12 @@
+let deckId = ''
+
 function handleClick() {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data)
+        deckId = data.deck_id
+    })
 }
 
 document.getElementById("new-deck").addEventListener('click', handleClick)
