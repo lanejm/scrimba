9e4bd62cf3ae537eca2ad2d718ac871c3ac9d4de
lanejm <67156874+lanejm@users.noreply.github.com>
2021-08-30 11:46:48 -0400

    cards showing on page

--- warCardGame/index.js
+++ warCardGame/index.js
@@ -1,21 +1,25 @@
-let deckId = ''
+let deckId = "";
+const showCards = document.getElementById("show-cards");
 
 function handleClick() {
-    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        deckId = data.deck_id
-    })
+  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      deckId = data.deck_id;
+    });
 }
 
-document.getElementById("new-deck").addEventListener('click', handleClick)
+document.getElementById("new-deck").addEventListener("click", handleClick);
 
 function drawCards() {
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.cards);
+      showCards.innerHTML = `<img src=${data.cards[0].image} />
+    <img src=${data.cards[1].image} />`;
+    });
 }
 
-document.getElementById('draw').addEventListener('click', drawCards)
+document.getElementById("draw").addEventListener("click", drawCards);
