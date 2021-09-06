61af68790afec99206ce2ff0a4d4349f96372d84
lanejm <67156874+lanejm@users.noreply.github.com>
2021-09-06 10:01:38 -0400

    async added

--- warCardGame/index.js
+++ warCardGame/index.js
@@ -8,41 +8,43 @@ let myScore = 0;
 const upperCard = document.getElementById("upper-card");
 const lowerCard = document.getElementById("lower-card");
 
-function handleClick() {
-  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
-    .then((response) => response.json())
-    .then((data) => {
-      remainingCards.textContent = `Remaining cards: ${data.remaining}`;
-      deckId = data.deck_id;
-    });
+async function handleClick() {
+  const response = await fetch(
+    "https://deckofcardsapi.com/api/deck/new/shuffle/"
+  );
+  const data = await response.json();
+  remainingCards.textContent = `Remaining cards: ${data.remaining}`;
+  deckId = data.deck_id;
+
+  // .then((response) => response.json())
+  // .then((data) => {;
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick);
 
-function drawCards() {
-  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-    .then((response) => response.json())
-    .then((data) => {
-      remainingCards.textContent = `Remaining Cards: ${data.remaining}`;
-      showCards.children[0].innerHTML = `
+async function drawCards() {
+  const response = await fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+  );
+  const data = await response.json();
+  remainingCards.textContent = `Remaining Cards: ${data.remaining}`;
+  showCards.children[0].innerHTML = `
       <img src=${data.cards[0].image} class="card" />`;
 
-      showCards.children[1].innerHTML = `
+  showCards.children[1].innerHTML = `
       <img src=${data.cards[1].image} class="card" />`;
 
-      const winnerText = handWinner(data.cards[0], data.cards[1]);
-      gameWinnerText.textContent = winnerText;
-      if (data.remaining === 0 && computerScore > myScore) {
-        gameWinnerText.textContent = "Computer wins this round!";
-        drawButton.disabled = true;
-      } else if (data.remaining === 0 && myScore > computerScore) {
-        gameWinnerText.textContent = "You win this round!";
-        drawButton.disabled = true;
-      } else {
-        gameWinnerText.textContent = "It's a tie!"
-        drawButton.disabled = false;
-      }
-    });
+  const winnerText = handWinner(data.cards[0], data.cards[1]);
+  gameWinnerText.textContent = winnerText;
+  if (data.remaining === 0 && computerScore > myScore) {
+    gameWinnerText.textContent = "Computer wins this round!";
+    drawButton.disabled = true;
+  } else if (data.remaining === 0 && myScore > computerScore) {
+    gameWinnerText.textContent = "You win this round!";
+    drawButton.disabled = true;
+  } else {
+    drawButton.disabled = false;
+  }
 }
 
 draw.addEventListener("click", drawCards);
