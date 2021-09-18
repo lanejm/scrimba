62a171a1327a904a9660228e0a1d9d68aa6e6ba2
lanejm <67156874+lanejm@users.noreply.github.com>
2021-09-18 10:41:06 -0400

    crypto info added

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,7 +1,8 @@
 imageAuthor = document.getElementById("image-author");
+cryptoTop = document.getElementById("crypto-top");
 
 fetch(
-  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=afjiodsafjoais"
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
   .then((res) => res.json())
   .then((data) => {
@@ -10,5 +11,14 @@ fetch(
   })
   .catch((err) => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1499002238440-d264edd596ec?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzE5MDcxMjA&ixlib=rb-1.2.1&q=80&w=1080)`;
-    imageAuthor.textContent = 'By: Léonard Cotte'
+    imageAuthor.textContent = "By: Léonard Cotte";
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => res.json())
+  .then((data) => {
+    cryptoTop.innerHTML = `
+   <img src=${data.image.small} />
+    <span>${data.name}</span>`;
+  })
+  .catch((err) => console.log(err));
