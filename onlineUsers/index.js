8b768cce6271ed1bd3388b42db4dd79546c8fae9
lanejm <67156874+lanejm@users.noreply.github.com>
2021-09-24 13:43:24 -0400

    chat app layout

--- onlineUsers/index.js
+++ onlineUsers/index.js
@@ -1,25 +1,26 @@
 async function getUsers() {
-    let response = await fetch("https://jsonplaceholder.typicode.com/users")
-    let users = await response.json()
-    return users
+  let response = await fetch("https://jsonplaceholder.typicode.com/users");
+  let users = await response.json();
+  return users;
 }
 
-
 function getUserDiv(user) {
-    return `
+  return `
     <div class="my-online-user">
     <div class="user-username">${user.username}</div>
     <div class="user-online"></div>
     </div>
-    `
+    `;
 }
-getUsers().then(users => {
-    let sampleUser = users[0]
-    let userDiv = getUserDiv(sampleUser)
-    
-    document.body.innerHTML = `<div class="my-online-users">
-        ${users.map(user => {
-            return getUserDiv(user)
-        }).join('')}
-    </div>`
-})
+getUsers().then((users) => {
+  let sampleUser = users[0];
+
+  document.body.innerHTML = `
+    <div class="header">Header</div>
+    <div class="my-online-users">
+        ${users.map((user) => getUserDiv(user)).join("")}
+    </div>
+    <div class="main-content">Main Content</div>
+    <div class="footer">Footer</div>
+    `;
+});
