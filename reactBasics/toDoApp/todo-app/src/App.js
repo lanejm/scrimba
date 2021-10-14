196b18937f7aee1081545ee6235309de81d580d8
lanejm <67156874+lanejm@users.noreply.github.com>
2021-10-14 19:03:42 -0400

    react todo app

---
+++ reactBasics/toDoApp/todo-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="checkbox"/>
+      <p>Placeholder text here</p>
+      <input type="checkbox"/>
+      <p>Placeholder text here</p>
+      <input type="checkbox"/>
+      <p>Placeholder text here</p>
+      <input type="checkbox"/>
+      <p>Placeholder text here</p>
+    </div>
+  );
+}
+
+export default App;
