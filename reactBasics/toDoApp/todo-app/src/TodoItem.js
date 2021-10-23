3a3de93ff178721ea7e0f7e184ffc28ba87b5fda
lanejm <67156874+lanejm@users.noreply.github.com>
2021-10-22 21:29:13 -0400

    conditional styling

--- reactBasics/toDoApp/todo-app/src/TodoItem.js
+++ reactBasics/toDoApp/todo-app/src/TodoItem.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 function ToDoItem(props) {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through",
+  };
   return (
     <div className="todo-item">
       <input
@@ -8,7 +13,9 @@ function ToDoItem(props) {
         checked={props.item.completed}
         onChange={() => props.handleChange(props.item.id)}
       />
-      <p>{props.item.text}</p>
+      <p style={props.item.completed ? completedStyle : null}>
+        {props.item.text}
+      </p>
     </div>
   );
 }
