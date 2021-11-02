f3e380dc8234618688691e783de74eae6ec2a012
lanejm <67156874+lanejm@users.noreply.github.com>
2021-11-02 13:55:18 -0400

    css

--- reactBasics/movieSearch/movie-search/src/index.js
+++ reactBasics/movieSearch/movie-search/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello world updated!</h1>
+      </div>
+    );
+  }
+}
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Main />, document.getElementById('root'));
