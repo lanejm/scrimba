45239b16ce9aafbff82fe88d6321f5a690e72478
lanejm <67156874+lanejm@users.noreply.github.com>
2021-11-28 14:00:43 -0500

    basic setup

--- advancedReact/speedTypingGame/speedtype/src/App.js
+++ advancedReact/speedTypingGame/speedtype/src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
 import './styles.css';
 
 function App() {
   return (
     <div>
+      <h1>How fast do you type?</h1>
+      <textarea></textarea>
+      <h4>Time Remaining: </h4>
+      <button>Start New Game</button>
+      <h1>Word Count: </h1>
     </div>
   );
 }
