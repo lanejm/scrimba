57d7564ebbcd40e6e8df466b9c2541c68266a5a6
lanejm <67156874+lanejm@users.noreply.github.com>
2021-11-30 15:12:52 -0500

    start button countdown

--- advancedReact/speedTypingGame/speedtype/src/App.js
+++ advancedReact/speedTypingGame/speedtype/src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 function App() {
   const [text, setText] = useState("");
   const [timeRemaining, setTimeRemaining] = useState(5);
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
 
   function handleChange(e) {
     const { value } = e.target;
@@ -17,19 +18,19 @@ function App() {
   }
 
   useEffect(() => {
-    if (timeRemaining > 0) {
+    if (isTimeRunning && timeRemaining > 0) {
       setTimeout(() => {
         setTimeRemaining((time) => time - 1);
       }, 1000);
     }
-  }, [timeRemaining]);
+  }, [timeRemaining, isTimeRunning]);
 
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text} />
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button onClick={() => wordCount(text)}>Start New Game</button>
+      <button onClick={() => setTimeRemaining(true)}>Start New Game</button>
       <h1>Word Count: </h1>
     </div>
   );
